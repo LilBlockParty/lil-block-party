cd0560291e2aef5460b33333adb92370c87f6895
mulford.eth <63870279+mhmulford0@users.noreply.github.com>
2022-09-15 19:44:39 -0500

    Add websockets & wallet connect (#1)
    * add providers
    
    * checkpoint
    
    * new btn styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,11 @@ import type { AppType } from "next/dist/shared/lib/utils";
 import { WagmiConfig, createClient, chain, configureChains } from "wagmi";
 import { infuraProvider } from "wagmi/providers/infura";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 
-const { provider, chains } = configureChains(
+const { provider, chains, webSocketProvider } = configureChains(
   [chain.mainnet],
-  [infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURA_KEY })]
+  [infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURA_KEY, stallTimeout: 2_000 })]
 );
 
 const client = createClient({
@@ -16,8 +17,15 @@ const client = createClient({
     new MetaMaskConnector({
       chains,
     }),
+    new WalletConnectConnector({
+      chains: [chain.mainnet],
+      options: {
+        qrcode: true,
+      },
+    }),
   ],
   provider,
+  webSocketProvider,
 });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
