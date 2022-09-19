a9f3c6d818abc4c60b5d1b4094da90605816938c
Matthew Mulford <mhmulford0@gmail.com>
2022-09-19 14:42:14 -0500

    misc copy. add twitter link. move lilnouns link to logo

--- src/components/InfoLil.tsx
+++ src/components/InfoLil.tsx
@@ -25,15 +25,19 @@ const InfoLil = ({ data, isFetching, isFetched }: Props) => {
     <div className="mx-auto max-w-2xl px-4 py-6 lg:max-w-6xl">
       <div className="w-full flex justify-between items-end">
         <span className="w-3/5">
-          <img src={Logo.src} alt="logo" />
-        </span>
-        <div className="flex justify-between items-end w-2/5">
-          <Link href="#wtf">
-            <a className="text-white text-2xl hover:underline">WTF?</a>
-          </Link>
           <Link href="https://lilnouns.wtf">
-            <a className="text-white text-2xl hover:underline">lilnouns.wtf</a>
+            <img src={Logo.src} alt="logo" />
           </Link>
+        </span>
+        <div className="flex justify-around items-end w-2/5">
+          <Link href="#wtf">
+            <a className="text-white text-2xl hover:underline mr-2 md:mr-0">WTF?</a>
+          </Link>
+
+          <Link href="https://twitter.com/lilblockparty">
+            <a className="text-white text-2xl hover:underline mr-2 md:mr-0">Twitter</a>
+          </Link>
+
           <span className="hidden md:block">
             <ConnectWalletBtn />
           </span>
@@ -42,7 +46,7 @@ const InfoLil = ({ data, isFetching, isFetched }: Props) => {
       <div className="flex flex-wrap items-start pt-8 ">
         <h1 className="text-5xl font-bold mb-2 text-[#F8F8F2] w-full">Lil&apos; Block Party </h1>
         <p className="font-bold text-[#92FFFF] text-2xl mb-6">
-          Be the first to see, settle, and bid on the next Lil Noun on the block!
+          Watch the blocks. Pick a lil. Join the party.
         </p>
       </div>
 
