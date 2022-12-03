9946a0c5915f3c77abac3b3cda8aacf4593a4a88
warmrome <pxpx786@gmail.com>
2022-12-03 14:03:12 -0800

    adding isLilNoundersToken function to AuctionBtn component. This adds the threshold logic as well.

--- frontend/src/components/AuctionBtn.tsx
+++ frontend/src/components/AuctionBtn.tsx
@@ -25,6 +25,16 @@ const AuctionBtn = ({ data, isFetching }: Props) => {
     write?.();
   };
 
+  // If token is under the threshhold and ends in 10, return true
+  // https://github.com/lilnounsDAO/lilnouns-monorepo/blob/59ff19a364e631d8e5484823e2982858d99daf8d/packages/nouns-contracts/contracts/NounsToken.sol#L165-L176
+  const isLilNoundersToken = () => {
+    if (data?.[1].lte(175300) && data?.[1].mod(10).isZero()) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   if (isConnected) {
     return (
       <button
@@ -37,7 +47,7 @@ const AuctionBtn = ({ data, isFetching }: Props) => {
           <span className="w-full text-3xl text-slate-500">
             Fetching Block...
           </span>
-        ) : data?.[1].mod(10).isZero() ? (
+        ) : isLilNoundersToken() ? (
           <span className="w-full text-3xl">I&apos;m feeling lucky</span>
         ) : (
           <span className="w-full text-3xl">Settle auction</span>
