63b09e48364d0d3222788c8eeb52ad8d1f1ddd37
Matthew Mulford <mhmulford0@gmail.com>
2023-02-05 17:49:09 -0600

    cache

--- frontend/src/pages/api/graveyard.ts
+++ frontend/src/pages/api/graveyard.ts
@@ -20,7 +20,7 @@ export default async function handler(
       const fmt = data.map((lil) => {
         return JSON.parse(lil);
       });
-      res.setHeader("Cache-Control", "public, s-maxage=10, stale-while-revalidate=59");
+      res.setHeader("Cache-Control", "max-age=120, stale-while-revalidate=240");
       res.status(200).send(fmt);
       break;
 
