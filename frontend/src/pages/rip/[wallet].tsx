86b6e3cfcad28f4b8c0b1614c4963ce4a9f3c457
Matthew Mulford <mhmulford0@gmail.com>
2023-02-05 19:33:08 -0600

    enhance wallet page

--- frontend/src/pages/rip/[wallet].tsx
+++ frontend/src/pages/rip/[wallet].tsx
@@ -6,6 +6,7 @@ import { z } from "zod";
 import Tombstone from "../../components/Tombstone";
 
 export type LilInfo = {
+  address: string;
   eulogy: string;
   imgData: string;
   tokenId: number;
@@ -14,18 +15,22 @@ export type LilInfo = {
 export default function RipPage() {
   const router = useRouter();
   const { wallet } = router.query;
-  const [lilInfo, setLilInfo] = useState<LilInfo>({
-    eulogy: "",
-    imgData: "",
-    tokenId: 0,
-  });
+  const [lilInfo, setLilInfo] = useState<LilInfo[]>([
+    {
+      address: "",
+      eulogy: "",
+      imgData: "",
+      tokenId: 0,
+    },
+  ]);
 
   useEffect(() => {
     if (!wallet) return;
     async function getEulogies() {
-      const res = await fetch("/api/graveyard", { body: JSON.stringify(wallet) });
+      const res = await fetch("/api/graveyard");
       if (res.ok) {
-        const data = await res.json();
+        const data: LilInfo[] = await res.json();
+        data.filter((lil) => lil.address);
         console.info(data, "set data");
         setLilInfo(data);
       }
@@ -36,33 +41,36 @@ export default function RipPage() {
   return (
     <>
       <Head>
-        <title>Title</title>
-        <meta property="og:image" content={`/api/og?wallet=${wallet}`} />
+        <title>Your Memories</title>
       </Head>
-      <div className="mx-auto flex flex-col pt-6 bg-[#22212C] min-h-screen">
-        <section className="max-w-7xl mx-auto mb-12">
-          <div className="flex flex-wrap w-full items-center">
-            <Tombstone />
+      <div className="mx-auto flex flex-wrap pt-6 bg-[#22212C] min-h-screen">
+        {lilInfo.map((lil) => {
+          return (
+            <>
+              <section className="mx-auto mb-12">
+                <div className="flex flex-wrap w-full items-center">
+                  <Tombstone />
 
-            <div className="ml-4">
-              <h3 className=" text-gray-400 text-3xl mt-4">
-                RIP to the almost Lil #: {lilInfo.tokenId}
-              </h3>
-              <span className="text-white text-xl block">{lilInfo?.eulogy}</span>
-            </div>
-          </div>
-        </section>
-        <div className="flex justify-center max-w-5xl mx-auto">
-          <section className="w-full mr-8">
-            <img
-              src={`data:image/svg+xml;base64,${lilInfo.imgData}`}
-              className=" object-cover object-center min-h-[500px] mr-auto rounded-md"
-              alt="lil"
-            />
-
-            {/* <PendingLil /> */}
-          </section>
-        </div>
+                  <div className="ml-4">
+                    <h3 className=" text-gray-400 text-3xl mt-4">
+                      RIP to the almost Lil #: {lil.tokenId}
+                    </h3>
+                    <span className="text-white text-xl block">{lil?.eulogy}</span>
+                  </div>
+                </div>
+                <div className="flex justify-center max-w-5xl mx-auto">
+                  <section className="w-full mr-8 mt-8">
+                    <img
+                      src={`data:image/svg+xml;base64,${lil.imgData}`}
+                      className=" object-cover object-center min-h-[500px] mr-auto rounded-md"
+                      alt="lil"
+                    />
+                  </section>
+                </div>
+              </section>
+            </>
+          );
+        })}
       </div>
     </>
   );
